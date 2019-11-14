30a4c3d568b5a5e876d1f0b69c3703247b64430a
hieudepchai <hieunguyenvn98@gmail.com>
2019-11-14 23:26:00 +0700

    update lab 2 3 5

--- Lab1/ex2.py
+++ Lab1/ex2.py
@@ -9,7 +9,16 @@ def remove_dollar(arg):
 df = pd.read_csv('sales.csv')
 df['ordered_at'] = pd.to_datetime(df['ordered_at'])
 print(df)
-df[['price','line_total']] = df[['price','line_total']].apply(remove_dollar, axis=1)
+# df[['price','line_total']] = df[['price','line_total']].apply(remove_dollar_2, axis=1)
+# for index, row in df.iterrows():
+#     df.loc[index,'price']= row['price'].replace("$","")
+#     df.loc[index,'line_total'] = row['line_total'].replace("$","")
+
+df['price'] = df['price'].str.replace("$","")
+df['line_total'] = df['line_total'].str.replace("$","")
+
 print(df)
 df[['price','line_total']] = df[['price','line_total']].astype("float")
 print(df.dtypes)
+x = df.duplicated()
+print(x)
